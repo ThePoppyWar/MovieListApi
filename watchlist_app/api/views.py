e075f45c6d05006f32f11c8c97d0dbe1b8bb6ae9
anna <anna.jablonska18@gmail.com>
2022-08-11 12:50:24 +0200

    error

--- watchlist_app/api/views.py
+++ watchlist_app/api/views.py
@@ -1,5 +1,6 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
 
 from watchlist_app.api.serializers import MovieSerializer
 from watchlist_app.models import Movie
@@ -24,7 +25,10 @@ def movie_list(request):
 @api_view(['GET', 'PUT', 'DELETE'])
 def movie_details(request, pk):
     if request.method == 'GET':
-        movie = Movie.objects.get(pk=pk)
+        try:
+            movie = Movie.objects.get(pk=pk)
+        except Movie.DoesNotExist:
+            return Response({'error': "Movie not found"}, status=status.HTTP_404_NOT_FOUND)
         serializer = MovieSerializer(movie)
         return Response(serializer.data)
 
@@ -35,10 +39,9 @@ def movie_details(request, pk):
             serializer.save()
             return Response(serializer.data)
         else:
-            return Response(serializer.errors)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-
-
-    # if request.method == 'DELETE':
-    #     pass
-
+    if request.method == 'DELETE':
+        movie = Movie.objects.get(pk=pk)
+        movie.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
