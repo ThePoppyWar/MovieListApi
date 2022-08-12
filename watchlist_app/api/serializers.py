21084047a7299f02afe5cf72be2d2d6776ba5067
anna <anna.jablonska18@gmail.com>
2022-08-12 14:47:32 +0200

    hej

--- watchlist_app/api/serializers.py
+++ watchlist_app/api/serializers.py
@@ -4,11 +4,16 @@ from watchlist_app.models import Movie
 
 
 class MovieSerializer(serializers.ModelSerializer):
+    len_name = serializers.SerializerMethodField()
     class Meta:
         model = Movie
         fields = '__all__'
         # fields = ['id', 'name', 'description']
         # exclude = ['name']
+        
+    def get_len_name(self, object):
+        length = len(object.name)
+        return length
 
     def validate(self, data):
         if data['name'] == data['description']:
