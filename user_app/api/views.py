443c0b6159c20cf61d48a5f7445f7c99d56e9c2e
anna <anna.jablonska18@gmail.com>
2022-09-20 21:30:22 +0200

    authomattoken2

--- user_app/api/views.py
+++ user_app/api/views.py
@@ -1,6 +1,9 @@
+from tokenize import Token
 from rest_framework.decorators import api_view
+from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
 from user_app.api.serializers import RegistrationSerializer
+from user_app import models
 
 
 @api_view(['POST',])
@@ -8,7 +11,22 @@ def registration_view(request):
 
     if request.method == 'POST':
         serializer = RegistrationSerializer(data=request.data)
+
+        data = {}
+
         if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
+            account = serializer.save()
+
+            data['response'] = 'Registration Successfully'
+            data['username'] = account.username
+            data['email'] = account.email
+
+            token = Token.objects.get(user=account).key
+            data['token'] = token
+
+        else:
+            data= serializer.errors
+
+
+            return Response(data)
             
