548ce2881e0bfc28831c9a600c6d3da2cd98c9e2
anna <anna.jablonska18@gmail.com>
2022-09-21 21:12:33 +0200

    logout

--- user_app/api/views.py
+++ user_app/api/views.py
@@ -1,9 +1,22 @@
 from tokenize import Token
 from rest_framework.decorators import api_view
 from rest_framework.authtoken.models import Token
+from rest_framework import status
 from rest_framework.response import Response
 from user_app.api.serializers import RegistrationSerializer
 from user_app import models
+
+
+
+@api_view(['POST',])
+def logout_view(request):
+
+    if request.method == 'POST':
+        request.user.auth_token.delete()
+        return Response(status=status.HTTP_200_OK)
+
+
+
 
 
 @api_view(['POST',])
@@ -28,5 +41,5 @@ def registration_view(request):
             data= serializer.errors
 
 
-            return Response(data)
+        return Response(data)
             
